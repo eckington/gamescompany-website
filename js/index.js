3cafe3ac9fa506d581489030884a793c8d2a9938
Hayden Young <hayden@haydennyyy.com>
2019-03-15 21:50:32 +0000

    Added a 16-line routing system with 404 handling.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,25 @@
 let dataObject = { siteName: 'GamesCompany' }
 
-let toRender = render('<h1>{{siteName}}</h1>', dataObject);
+let fourohfour = [
+  '<h1>404 error! Sorry!</h1>',
+  '<h2>That page wasn\'t found.</h2>'
+];
 
-document.getElementById('application_main').innerHTML = toRender;
+function route() {
+  let path = '../tmpl/' + window.location.hash.substring(2) + '.tmpl'
+
+  let req = new XMLHttpRequest();
+      req.open('GET', path, true);
+      req.send();
+
+  req.onloadend = () => {
+    if(req.status == 404) {
+      document.getElementById('app').innerHTML = render(fourohfour.join('\n'));
+    } else {
+      console.log(req.responseText);
+      document.getElementById('app').innerHTML = render(req.responseText, dataObject);
+    }
+  }
+}
+route();
+window.addEventListener('hashchange', route);
