25f7a0b1f5558b091209da098be0cdc8e5037ae5
Hayden Young <hayden@haydennyyy.com>
2019-03-16 17:54:14 +0000

    Minified dataObject, added titles per route

--- js/router.js
+++ js/router.js
@@ -1,56 +1,4 @@
-let dataObject = {
-  siteName: 'GamesCompany',
-  xboxGames: 'Hello',
-  pcGames: 'PC',
-  ps4Games: 'PS4',
-  games: {
-    pc: [
-      {
-        name: 'Just Cause 4',
-        image: 'https://via.placeholder.com/150x200.png',
-        price: 34.55,
-        publisher: 'Avalanche Studios',
-        market: 'steam',
-        link: '#'
-      },
-      {
-        name: 'PC Game',
-        image: 'https://via.placeholder.com/150x200.png',
-        link: '#'
-      },
-      {
-        name: 'PC Game',
-        image: 'https://via.placeholder.com/150x200.png',
-        link: '#'
-      },
-      {
-        name: 'PC Game',
-        image: 'https://via.placeholder.com/150x200.png',
-        link: '#'
-      },
-      {
-        name: 'PC Game',
-        image: 'https://via.placeholder.com/150x200.png',
-        link: '#'
-      },
-      {
-        name: 'PC Game',
-        image: 'https://via.placeholder.com/150x200.png',
-        link: '#'
-      },
-      {
-        name: 'PC Game',
-        image: 'https://via.placeholder.com/150x200.png',
-        link: '#'
-      },
-      {
-        name: 'PC Game',
-        image: 'https://via.placeholder.com/150x200.png',
-        link: '#'
-      }
-    ]
-  }
-}
+let dataObject = {siteName:"GamesCompany",xboxGames:"Hello",pcGames:"PC",ps4Games:"PS4",games:{pc:[{name:"Just Cause 4",image:"https://via.placeholder.com/150x200.png",price:34.55,publisher:"Avalanche Studios",market:"steam",link:"#"},{name:"PC Game",image:"https://via.placeholder.com/150x200.png",link:"#"},{name:"PC Game",image:"https://via.placeholder.com/150x200.png",link:"#"},{name:"PC Game",image:"https://via.placeholder.com/150x200.png",link:"#"},{name:"PC Game",image:"https://via.placeholder.com/150x200.png",link:"#"},{name:"PC Game",image:"https://via.placeholder.com/150x200.png",link:"#"},{name:"PC Game",image:"https://via.placeholder.com/150x200.png",link:"#"},{name:"PC Game",image:"https://via.placeholder.com/150x200.png",link:"#"}],ps4:[{name:"Just Cause 4",image:"https://via.placeholder.com/150x200.png",price:34.55,publisher:"Avalanche Studios",market:"steam",link:"#"},{name:"PC Game",image:"https://via.placeholder.com/150x200.png",link:"#"},{name:"PC Game",image:"https://via.placeholder.com/150x200.png",link:"#"},{name:"PC Game",image:"https://via.placeholder.com/150x200.png",link:"#"},{name:"PC Game",image:"https://via.placeholder.com/150x200.png",link:"#"},{name:"PC Game",image:"https://via.placeholder.com/150x200.png",link:"#"},{name:"PC Game",image:"https://via.placeholder.com/150x200.png",link:"#"},{name:"PC Game",image:"https://via.placeholder.com/150x200.png",link:"#"}],xbox:[{name:"Just Cause 4",image:"https://via.placeholder.com/150x200.png",price:34.55,publisher:"Avalanche Studios",market:"xbox",link:"#"},{name:"PC Game",image:"https://via.placeholder.com/150x200.png",link:"#"},{name:"PC Game",image:"https://via.placeholder.com/150x200.png",link:"#"},{name:"PC Game",image:"https://via.placeholder.com/150x200.png",link:"#"},{name:"PC Game",image:"https://via.placeholder.com/150x200.png",link:"#"},{name:"PC Game",image:"https://via.placeholder.com/150x200.png",link:"#"},{name:"PC Game",image:"https://via.placeholder.com/150x200.png",link:"#"},{name:"PC Game",image:"https://via.placeholder.com/150x200.png",link:"#"}]}};
 
 let fourohfour = [
   '<div class="container">',
@@ -60,13 +8,13 @@ let fourohfour = [
 ];
 
 let routes = [
-  { route: '/', file: 'index' },
-  { route: '/reviews', file: 'reviews' },
-  { route: '/games', file: 'games' },
-  { route: '/games/pc', file: 'games-pc' },
-  { route: '/games/ps4', file: 'games-ps4' },
-  { route: '/games/xbox', file: 'games-xbox' },
-  { route: '/search', file: 'search' }
+  { route: '/', file: 'index', title: 'Home' },
+  { route: '/reviews', file: 'reviews', title: 'Reviews' },
+  { route: '/games', file: 'games', title: 'Games' },
+  { route: '/games/pc', file: 'games-pc', title: 'PC Games' },
+  { route: '/games/ps4', file: 'games-ps4', title: 'PS4 Games' },
+  { route: '/games/xbox', file: 'games-xbox', title: 'Xbox Games' },
+  { route: '/search', file: 'search', title: 'Search' }
 ];
 
 /**
@@ -95,12 +43,15 @@ function route() {
       
       // ... render the 404 template (see top of file).
       document.getElementById('app').innerHTML = render(fourohfour.join('\n'));
+      document.title = `GamesCompany - 404`;
 
     // But if it can...
     } else {
       
       // ... render it
       document.getElementById('app').innerHTML = render(req.responseText, dataObject);
+      document.title = `GamesCompany - ${routeFile.title}`;
+
     }
   }
 }
