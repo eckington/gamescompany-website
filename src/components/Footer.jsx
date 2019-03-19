401fa4adf92d3e6fd07ac9fbd95f1a1e658ddcc1
Hayden Young <hayden@haydennyyy.com>
2019-03-19 17:02:49 +0000

    Fixed the header & added the footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div className="container">
+          <p className="copyright">Copyright &copy; GamesCompany 2019. All rights reserved.</p>
+        </div>
+      </footer>
+    )
+  }
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
   render() {
     return(
       <header>
-        <div class="container">
-          <div class="topRow">
+        <div className="container">
+          <div className="topRow">
             <a href="#/">
               <img src="https://via.placeholder.com/250x130.png" alt="Logo" />
             </a>
-            <p class="tagline">Where even Cowell would wet his pants</p>
+            <p className="tagline">Reviews, comments &amp; nasty people!</p>
           </div>
-          <hr>
-          <div class="links">
-            <span class="menu-icon">Menu</span>
-            <ul class="links-list">
-              <li><a href="#/">Home</a></li>
-              <li><a href="#/games/pc">PC Games</a></li>
-              <li><a href="#/games/ps4">PS4 Games</a></li>
-              <li><a href="#/games/xbox">Xbox One Games</a></li>
-              <li><a href="#/reviews">Reviews</a></li>
-              <a href="#/search" class="search-link">Search</a>
+          <hr/>
+          <div className="links">
+            <span className="menu-icon">Menu</span>
+            <ul className="links-list">
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/games/pc">PC Games</Link></li>
+              <li><Link to="/games/ps4">PS4 Games</Link></li>
+              <li><Link to="/games/xbox">Xbox One Games</Link></li>
+              <li><Link to="/reviews">Reviews</Link></li>
+              <Link to="/search" className="search-link">Search</Link>
             </ul>
           </div>
         </div>
