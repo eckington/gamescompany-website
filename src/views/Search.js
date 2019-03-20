1d6b398d109e4d529490024671967dbe180bd903
Hayden Young <hayden@haydennyyy.com>
2019-03-20 08:32:10 +0000

    Began implementation of search

--- src/views/Search.js
+++ src/views/Search.js
@@ -1,14 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import games from '../data/games.json';
 
 export default class Search extends Component {
+  constructor(...args) {
+    super(...args);
+    this.state = { games: games, reviews: [], posts: [] };
+    this.submit.bind(this);
+    console.log(this);
+  }
+
+  componentDidMount() {
+    this.setState({ games: games, loaded: true });
+  }
+
+  submit(ev) {
+    ev.preventDefault();
+    let searchText = document.getElementsByName('search').item(0).value;
+    let game = this.state.games.find(g => g.name.toLowerCase() == searchText.toLowerCase());
+  }
+
   render() {
-    return (
-      <div className="searchPage">
-        <form onSubmit={this.submit}>
-          <input name="search" placeholder="Search..." type="text" />
-          <input name="submit" value="Search" type="submit" />
-        </form>
-      </div>
-    )
+    const loaded = this.state.loaded;
+    if(!loaded) {
+      return (
+        <div className="container">
+          <h1>Loading...</h1>
+        </div>
+      )
+    } else {
+      return (
+        <div className="searchPage">
+          <form onSubmit={this.submit}>
+            <input name="search" placeholder="Search..." type="text" />
+            <input name="submit" value="Search" type="submit" />
+          </form>
+        </div>
+      )
+    }
   }
 }
