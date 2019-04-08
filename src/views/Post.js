0d0a24c6772fd877c9acc57bb56a4f18bdd824e8
Hayden Young <hayden@haydennyyy.com>
2019-04-08 14:28:00 +0100

    Removed the old router & templating engine from before, it's not needed.

--- src/views/Post.js
+++ src/views/Post.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import reviewsList from '../data/reviews.json';
+import React, { Component } from "react";
+import reviewsList from "../data/reviews.json";
 // import postsList from '../data/posts.json';
 
 export default class Post extends Component {
-  state = { post: { title: '', content: [] } };
+  state = { post: { title: "", content: [] } };
 
   componentDidMount() {
     const shortName = this.props.location.pathname.substring(
@@ -11,7 +11,7 @@ export default class Post extends Component {
     );
 
     const post =
-      this.props.type === 'review'
+      this.props.type === "review"
         ? reviewsList.find(e => e.shortName == shortName)
         : /* postsList.find(e => e.shortName == shortName) */ null;
 
@@ -25,7 +25,7 @@ export default class Post extends Component {
      *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
      *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
     var disqus_config = function() {
-      this.page.url = `https://r085.haydennyyy.com/#/${
+      this.page.url = `https://imedia-r085.netlify.com/#/${
         this.props.location.pathname
       }`; // Replace PAGE_URL with your page's canonical URL variable
       this.page.identifier = this.props.location.pathname; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
@@ -33,9 +33,9 @@ export default class Post extends Component {
     (function() {
       // DON'T EDIT BELOW THIS LINE
       var d = document,
-        s = d.createElement('script');
-      s.src = 'https://imedia-r085.disqus.com/embed.js';
-      s.setAttribute('data-timestamp', +new Date());
+        s = d.createElement("script");
+      s.src = "https://imedia-r085.disqus.com/embed.js";
+      s.setAttribute("data-timestamp", +new Date());
       (d.head || d.body).appendChild(s);
     })();
   }
@@ -50,9 +50,9 @@ export default class Post extends Component {
       document.body.style = `background:url(${
         this.state.post.image
       });background-attachment:fixed;background-position:center;background-repeat:no-repeat;background-size:cover`;
-      const title = this.state.post.title.split(' ');
+      const title = this.state.post.title.split(" ");
       const lastWord = title.pop();
-      const titleToDisplay = title.join(' ');
+      const titleToDisplay = title.join(" ");
       return (
         <div className="post container">
           <div className="content">
@@ -69,7 +69,7 @@ export default class Post extends Component {
       );
     } else {
       const FourOhFour = ({ location }) => (
-        <div className="container" style={{ textAlign: 'center' }}>
+        <div className="container" style={{ textAlign: "center" }}>
           <h3>404.</h3>
           <h4>That's an error.</h4>
         </div>
